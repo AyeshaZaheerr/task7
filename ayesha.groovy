de066d7205d44537eddcbc89d916ce57bb13c444
AyeshaZaheerr <111960962+AyeshaZaheerr@users.noreply.github.com>
2023-10-25 08:29:50 +0500

    Create ayesha.groovy

---
+++ ayesha.groovy
@@ -0,0 +1,8 @@
+def FunctionProd(String S) {
+    if (S == 'PROD') {
+        println("Deploying to production....")
+    } else {
+        println("Deploying to testing......")
+    }
+}
+return this
